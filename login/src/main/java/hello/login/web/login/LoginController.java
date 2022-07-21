dd294b70a1711e809f8c3c56dc0316858a7a4114
seheon <quwe153@naver.com>
2022-07-21 14:28:58 +0900

    섹션 7. 로그인 처리2 - 필터, 인터셉터

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResultUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -52,7 +53,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("login")
+//    @PostMapping("login")
     public String loginV2(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request){
         if (bindingResult.hasErrors()){
             return "/login/loginForm";
@@ -72,6 +73,31 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+    @PostMapping("login")
+    public String loginV3(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request){
+        if (bindingResult.hasErrors()){
+            return "/login/loginForm";
+        }
+
+        Member member = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if (member == null){
+            bindingResult.reject("loginFail", "아이디 혹은 비밀번호가 맞지 않습니다.");
+            return "/login/loginForm";
+        }
+
+        // 로그인 성공 처리 TODO
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, member);
+
+        return "redirect:" + redirectURL;
+    }
+
+
 
 //    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
